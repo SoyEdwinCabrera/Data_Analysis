c24454bbdc337ca70149c4ec0b7eaf2a3d4b6f45
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-07-08 22:34:07 -0500

    Update(2-av-rat-week.py): Add explanation about the functions of the script

--- 2-av-rat-week.py
+++ 2-av-rat-week.py
@@ -4,10 +4,16 @@ from datetime import datetime
 from pytz import utc
 import matplotlib.pyplot as plt
 
+# Carga los datos desde un archivo CSV y convierte la columna 'Timestamp' a formato de fecha
 data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
+
+# Agrega una nueva columna 'Week' que contiene el año y el número de la semana
 data['Week'] = data['Timestamp'].dt.strftime('%Y-%U')
+
+# Agrupa los datos por semana y calcula el promedio de las calificaciones
 week_average = data.groupby('Week')['Rating'].mean()
 
+# Definición del gráfico en formato JSON con el formato de manejo de https://www.highcharts.com/
 chart_def = """
 {
     chart: {
@@ -72,15 +78,25 @@ chart_def = """
 }
 """
 
+# Función principal que define la aplicación web
 def app():
+    # Crea una página Quasar, que es la base de la interfaz web
     wp = jp.QuasarPage()
+    
+    # Agrega un encabezado con texto centrado
     h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    
+    # Agrega una descripción debajo del encabezado
     p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis.")
     
-    hc = jp.HighCharts(a=wp, options= chart_def)
-    hc.options.xAxis.categories = list(week_average.index)
-    hc.options.series[0].data = list(week_average)
+    # Agrega un gráfico interactivo basado en HighCharts
+    hc = jp.HighCharts(a=wp, options=chart_def)
+    
+    # Configura los datos del gráfico
+    hc.options.xAxis.categories = list(week_average.index)  # Semanas en el eje X
+    hc.options.series[0].data = list(week_average)  # Promedios de calificaciones en el eje Y
     
     return wp
-    
+
+# Ejecuta la aplicación en el puerto 8080
 jp.justpy(app, port=8080)
