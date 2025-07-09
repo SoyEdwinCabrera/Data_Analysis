8a1408e6848815a961088b81cbfe0af06fbfd62a
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-07-08 22:34:56 -0500

    Update(3-av-rat-month.py): Add explanation about the functions of the script

--- 3-av-rat-month.py
+++ 3-av-rat-month.py
@@ -2,11 +2,17 @@ import justpy as jp
 import pandas
 from datetime import datetime
 from pytz import utc
- 
+
+# Carga los datos desde un archivo CSV y convierte la columna 'Timestamp' a formato de fecha
 data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
+
+# Agrega una nueva columna 'Month' que contiene el año y el mes en formato 'YYYY-MM'
 data['Month'] = data['Timestamp'].dt.strftime('%Y-%m')
+
+# Agrupa los datos por mes y calcula el promedio de las calificaciones
 month_average = data.groupby('Month')['Rating'].mean()
- 
+
+# Definición del gráfico en formato JSON con el formato de manejo de https://www.highcharts.com/
 chart_def = """
 {
     chart: {
@@ -67,16 +73,26 @@ chart_def = """
     }]
 }
 """
- 
+
+# Función principal que define la aplicación web
 def app():
+    # Crea una página Quasar, que es la base de la interfaz web
     wp = jp.QuasarPage()
+    
+    # Agrega un encabezado con texto centrado
     h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    
+    # Agrega una descripción debajo del encabezado
     p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
     
+    # Agrega un gráfico interactivo basado en HighCharts
     hc = jp.HighCharts(a=wp, options=chart_def)
-    hc.options.xAxis.categories = list(month_average.index)
-    hc.options.series[0].data = list(month_average)
- 
+    
+    # Configura los datos del gráfico
+    hc.options.xAxis.categories = list(month_average.index)  # Meses en el eje X
+    hc.options.series[0].data = list(month_average)  # Promedios de calificaciones en el eje Y
+    
     return wp
- 
-jp.justpy(app, port =8080)
+
+# Ejecuta la aplicación en el puerto 8080
+jp.justpy(app, port=8080)
